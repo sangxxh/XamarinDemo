b3a9da9500f8f2c1e33c8aae5796223cd69e64c7
tomasfil <34779382+tomasfil@users.noreply.github.com>
2022-04-06 14:03:53 +0200

    Fix VM xmlns (#1)
    Co-authored-by: Tomáš Filip <tomas.filip@meac.cz>

--- Mobile/Mobile/MainPage.xaml.cs
+++ Mobile/Mobile/MainPage.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using ReactiveUI.XamForms;
+using Xamarin.Forms.Xaml;
 
 namespace Mobile
 {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainPage : ReactiveContentPage<MainPageViewModel>
     {
         public MainPage()
